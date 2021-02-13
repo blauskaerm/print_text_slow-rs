299ee83611b6bc33e4c44567b01ee5abe913d951
BlauskaerM <blauskaerm@protonmail.ch>
2021-02-13 23:59:57 +0100

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use std::process;
 use std::thread;
 use std::time;
 
+extern crate clap;
+use clap::{App, Arg};
+
 macro_rules! sleep {
     ($duration:expr) => {{
         let ten_millis = time::Duration::from_millis($duration);
@@ -42,12 +45,45 @@ const BACKSPACE: char = 8 as char;
 const SPACE: char = 32 as char;
 
 fn main() {
-    let filename = String::from("src/main.rs");
+    let cmd_options = App::new("print_text_slow")
+        .version("0.1")
+        .author("BlauskaerM <blauskaerm@protonmail.ch>")
+        .about(
+            "Print the source code of your projects and watch your computer think while doing it",
+        )
+        .arg(
+            Arg::with_name("FILE")
+                .help("File to print")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("Phrases")
+                .short("p")
+                .long("phrase")
+                .value_name("file-path")
+                .help("File containing a list of phrases (default ./phrase)")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Answers")
+                .short("a")
+                .long("answer")
+                .value_name("file-path")
+                .help("File containing a list of answers (default ./answer)")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let filename = cmd_options.value_of("FILE").unwrap_or("NOT FILE SELECTED");
+    let phrases_file = cmd_options.value_of("Phrases").unwrap_or("phrase");
+    let answers_file = cmd_options.value_of("Answers").unwrap_or("answer");
+
     let mut phrase: Vec<String> = Vec::new();
     let mut answer: Vec<String> = Vec::new();
 
-    read_thinking_alternatives("phrase", &mut phrase);
-    read_thinking_alternatives("answer", &mut answer);
+    read_thinking_alternatives(phrases_file, &mut phrase);
+    read_thinking_alternatives(answers_file, &mut answer);
 
     let source_file_content =
         fs::read_to_string(&filename).expect(format!("Unable to read file {}", &filename).as_str());
