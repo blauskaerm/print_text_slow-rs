03b9f2de9eb6ea58e992d9f53faab60ddc8a56c8
BlauskaerM <blauskaerm@protonmail.ch>
2021-02-15 23:04:29 +0100

    Very much WIP but I have a client and server working

--- src/client/main.rs
+++ src/client/main.rs
@@ -10,26 +10,29 @@ use std::time;
 extern crate clap;
 use clap::{App, Arg};
 
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
 macro_rules! sleep {
     ($duration:expr) => {{
-        let ten_millis = time::Duration::from_millis($duration);
-        thread::sleep(ten_millis);
+	let ten_millis = time::Duration::from_millis($duration);
+	thread::sleep(ten_millis);
     }};
 }
 
 fn read_thinking_alternatives(filename: &str, storage_vec: &mut Vec<String>) {
     let file: std::fs::File;
     match File::open(filename) {
-        Ok(fd) => file = fd,
-        Err(err) => {
-            eprint!("Unable to open {}, {}", filename, err.to_string());
-            process::exit(-1);
-        }
+	Ok(fd) => file = fd,
+	Err(err) => {
+	    eprint!("Unable to open {}, {}", filename, err.to_string());
+	    process::exit(-1);
+	}
     };
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        storage_vec.push(String::from(line.unwrap()));
+	storage_vec.push(String::from(line.unwrap()));
     }
 }
 
@@ -45,35 +48,53 @@ const BACKSPACE: char = 8 as char;
 const SPACE: char = 32 as char;
 
 fn main() {
+    let mut stream = TcpStream::connect("localhost:7878").unwrap();
+
+    let mut buffer_string = String::new();
+
+    let result = stream.read_to_string(&mut buffer_string);
+
+    let answer_vec_test: Vec<String> = buffer_string
+	.split('\n')
+	.map(str::to_string)
+	.filter(|e| e.len() > 0)
+	.collect();
+
+    for my_string in answer_vec_test {
+	println!("Received: {}", my_string);
+    }
+
+    process::exit(0);
+
     let cmd_options = App::new("print_text_slow")
-        .version("0.1")
-        .author("BlauskaerM <blauskaerm@protonmail.ch>")
-        .about(
-            "Print the source code of your projects and watch your computer think while doing it",
-        )
-        .arg(
-            Arg::with_name("FILE")
-                .help("File to print")
-                .required(true)
-                .index(1),
-        )
-        .arg(
-            Arg::with_name("Phrases")
-                .short("p")
-                .long("phrase")
-                .value_name("file-path")
-                .help("File containing a list of phrases (default ./phrase)")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("Answers")
-                .short("a")
-                .long("answer")
-                .value_name("file-path")
-                .help("File containing a list of answers (default ./answer)")
-                .takes_value(true),
-        )
-        .get_matches();
+	.version("0.1")
+	.author("BlauskaerM <blauskaerm@protonmail.ch>")
+	.about(
+	    "Print the source code of your projects and watch your computer think while doing it",
+	)
+	.arg(
+	    Arg::with_name("FILE")
+		.help("File to print")
+		.required(true)
+		.index(1),
+	)
+	.arg(
+	    Arg::with_name("Phrases")
+		.short("p")
+		.long("phrase")
+		.value_name("file-path")
+		.help("File containing a list of phrases (default ./phrase)")
+		.takes_value(true),
+	)
+	.arg(
+	    Arg::with_name("Answers")
+		.short("a")
+		.long("answer")
+		.value_name("file-path")
+		.help("File containing a list of answers (default ./answer)")
+		.takes_value(true),
+	)
+	.get_matches();
 
     let filename = cmd_options.value_of("FILE").unwrap_or("NOT FILE SELECTED");
     let phrases_file = cmd_options.value_of("Phrases").unwrap_or("phrase");
@@ -86,35 +107,35 @@ fn main() {
     read_thinking_alternatives(answers_file, &mut answer);
 
     let source_file_content =
-        fs::read_to_string(&filename).expect(format!("Unable to read file {}", &filename).as_str());
+	fs::read_to_string(&filename).expect(format!("Unable to read file {}", &filename).as_str());
 
     let mut limit = 1;
     for source_file_line in source_file_content.lines() {
-        for char_in_file in source_file_line.chars() {
-            eprint!("{}", char_in_file);
-            sleep!(20);
-        }
-        eprintln!();
-        if limit == LIMIT {
-            let phrase_str = get_random_str(&mut phrase);
-            let answer_str = get_random_str(&mut answer);
-            let chars_to_delete = phrase_str.len() + answer_str.len() + 4;
+	for char_in_file in source_file_line.chars() {
+	    eprint!("{}", char_in_file);
+	    sleep!(20);
+	}
+	eprintln!();
+	if limit == LIMIT {
+	    let phrase_str = get_random_str(&mut phrase);
+	    let answer_str = get_random_str(&mut answer);
+	    let chars_to_delete = phrase_str.len() + answer_str.len() + 4;
 
-            eprint!("{}", Colour::Red.bold().paint(phrase_str));
-            for _i in 0..3 {
-                eprint!("{}", Colour::Red.bold().paint("."));
-                sleep!(1000);
-            }
-            eprint!("{}", Colour::Red.bold().paint(answer_str));
-            sleep!(2800);
+	    eprint!("{}", Colour::Red.bold().paint(phrase_str));
+	    for _i in 0..3 {
+		eprint!("{}", Colour::Red.bold().paint("."));
+		sleep!(1000);
+	    }
+	    eprint!("{}", Colour::Red.bold().paint(answer_str));
+	    sleep!(2800);
 
-            for _i in 1..chars_to_delete {
-                eprint!("{}", BACKSPACE);
-                eprint!("{}", SPACE);
-                eprint!("{}", BACKSPACE);
-            }
-            limit = 0;
-        }
-        limit += 1;
+	    for _i in 1..chars_to_delete {
+		eprint!("{}", BACKSPACE);
+		eprint!("{}", SPACE);
+		eprint!("{}", BACKSPACE);
+	    }
+	    limit = 0;
+	}
+	limit += 1;
     }
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,4 +1,53 @@
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::process;
+
+fn handle_client(mut stream: TcpStream) {
+    // let mut string_vec: Vec<String> = Vec::new();
+    // string_vec.push("Test".to_string());
+    // string_vec.push("Test".to_string());
+    // string_vec.push("Test".to_string());
+
+    let filename = "answer";
+    let file: std::fs::File;
+    match File::open(filename) {
+        Ok(fd) => file = fd,
+        Err(err) => {
+            eprint!("Unable to open {}, {}", filename, err.to_string());
+            process::exit(-1);
+        }
+    };
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        //storage_vec.push(String::from(line.unwrap()));
+        //println!("Sending string: {}", line);
+        let line = line.unwrap();
+        println!("Sending: {}", &line);
+        let message_buffer = format!("{}\n", line);
+        stream.write(message_buffer.as_bytes()).unwrap();
+    }
+
+    // Send EOF
+    //stream.write(&[0x04]);
+    stream.flush().unwrap();
+}
 
 fn main() {
-    println!("Hello world");
+    const BIND_ADDR: &str = "localhost";
+    const BIND_PORT: &str = "7878";
+
+    let listener = TcpListener::bind(format!("{}:{}", BIND_ADDR, BIND_PORT)).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+
+        handle_client(stream);
+    }
 }
